d980d1569dc5057bec083dc0aafe22840bcc0749
Sanil Doshi <goatdev2001@gmail.com>
2024-01-31 01:44:58 -0800

    Optimized code

--- src/analysis/pair_analysis.py
+++ src/analysis/pair_analysis.py
@@ -17,77 +17,74 @@ class PairAnalysis:
         data_fetcher = DataFetcher(tickers)
 
         self.data = data_fetcher.fetch_data(start_date="2020-10-27", end_date="2021-10-27")
-        # self.logger.debug(f"Type of fetched data: {type(self.data)}")
-
         self.logger.debug("Data loaded successfully")
 
-    def calculate_spread(self, pair):
-        """
-        Calculates the spread between two stocks.
+    # handle errors 
+    def handle_error(self, pairs_info=None):
+        self.logger.error("No pairs found")
+        print('\n')
+        if pairs_info:
+            for pair, value in pairs_info.items():
+                print(f"{pair[0]}, {pair[1]}: {value:.4f}")
+        sys.exit(1)
+    
+    def print_pairs(self, pairs, dictionary):
+        for pair in pairs:
+            print(f"{pair[0]}, {pair[1]}: {dictionary[pair]:.4f}")
 
-        :param pair: A tuple containing the two stock symbols.
-        :return: A pandas Series containing the spread.
-        """
+
+    def calculate_spread(self, pair):
+        # :param pair: A tuple containing the two stock symbols.
+
         self.logger.info(f"Calculating spread for {pair[0]} and {pair[1]}")
         spread = self.data[pair[0]] - self.data[pair[1]]
-        return spread
+        return spread # return pandas series containing spread
     
+    # correlation matrix calculation
     def correlation_matrix(self):
-        """
-        Calculates the correlation matrix for the data.
-
-        :return: A pandas DataFrame containing the correlation matrix.
-        """
         self.logger.info("Calculating correlation matrix")
-        return self.data.corr()
+        return self.data.corr() # return pandas dataframe 
 
     def potential_pairs(self, threshold=0.95):
         """
         Identifies potential pairs of stocks based on the correlation matrix.
 
-        :param correlation_matrix: A pandas DataFrame containing the correlation matrix.
         :param threshold: The minimum correlation value to consider a pair.
         :return: A list of tuples containing the potential pairs.
         """
-        correlation_matrix = self.correlation_matrix()
         self.logger.info(f"Identifying potential pairs with threshold {threshold}")
-        pairs = [] # list of identified pairs
+
+        correlation_matrix = self.correlation_matrix()
+        pairs = [] # list of valid pairs
         coefficients = {} # store coefficients for each pair
 
         for i in range(len(correlation_matrix.columns)):
             for j in range(i):
                 corr_value = abs(correlation_matrix.iloc[i, j]) # correlation coefficient
                 pair = (correlation_matrix.columns[i], correlation_matrix.columns[j]) # identify pair
-
                 coefficients[pair] = corr_value # store coefficient for pair
                 
                 if corr_value > threshold:
                     pairs.append(pair) # add pair to list
-                    self.logger.debug(f"Pair found: {correlation_matrix.columns[i]} and {correlation_matrix.columns[j]}")
+                    self.logger.debug(f"Pair found: {pair[0]} and {pair[1]}")
         
         if not pairs:
-            self.logger.error("No potential pairs found")
-            print("No potential pairs found, try changing the threshold or testing different stocks.")
-            for pair, coefficient in coefficients.items():
-                print(f"{pair[0]}, {pair[1]}: {coefficient:.4f}")
-            sys.exit(1)
+            self.handle_error(coefficients)
         else:
-            print(f"\nIdentified Pairs with Correlation Coefficients > {threshold}:")
-            for pair in pairs:
-                print(f"{pair[0]}, {pair[1]}: {coefficients[pair]:.4f}") # print valid pairs and their coefficients
+            self.print_pairs(pairs, coefficients)
             return pairs
-    
-    @staticmethod
-    def check_cointegration(spread, pair):
+        
+    def check_cointegration(self, spread, pair):
         """
-        Static method to perform Augmented Dickey-Fuller test.
+        Method to check cointegration by performing Augmented Dickey-Fuller test.
         """
         # perform the Augmented Dickey-Fuller test
         try:
+            self.logger.debug(f"Performing ADF test for {pair[0]} and {pair[1]}")
             adf_result = adfuller(spread)
             return adf_result[1]
         except Exception as e:
-            logging.error(f"Error performing ADF test for {pair[0]} and {pair[1]}: {e}")
+            self.logger.error(f"Error performing ADF test for {pair[0]} and {pair[1]}: {e}")
             return None
 
     def cointegration_test(self):
@@ -105,23 +102,18 @@ class PairAnalysis:
 
         for pair in pairs:
             spread = self.calculate_spread(pair)
-            p_value = PairAnalysis.check_cointegration(spread, pair)
-            pairs_info[pair] = p_value
+            p_value = self.check_cointegration(spread, pair)
+            pairs_info[pair] = p_value # add p-value to dictionary
+            
             if p_value < 0.05:
                 cointegrated_pairs.append(pair)
-                self.logger.debug(f"Pair {pair[0]} and {pair[1]} cointegrated. P-value: {p_value:.2f}")
+                self.logger.debug(f"{pair[0]}, {pair[1]}: Cointegrated (p-value: {p_value:.2f})")
             else:
-                self.logger.debug(f"Pair {pair[0]} and {pair[1]} not cointegrated. P-value: {p_value:.2f}")
+                self.logger.debug(f"{pair[0]}, {pair[1]}: Not cointegrated (p-value: {p_value:.2f})")
         
         if not cointegrated_pairs:
-            self.logger.error("No cointegrated pairs found")
-            print("No cointegrated pairs found (Need p-value < 0.05). Full list of pairs and p-values:")
-            for pair, p_value in pairs_info.items():
-                print(f"{pair[0]}, {pair[1]}: {p_value:.4f}")
-            sys.exit(1)
+            self.handle_error(pairs_info)
         else: 
-            print("\nCointegrated Pairs with P-value < 0.05:")
-            for pair in cointegrated_pairs:
-                print(f"{pair[0]}, {pair[1]}: {pairs_info[pair]:.4f}")
+            self.print_pairs(cointegrated_pairs, pairs_info)
             return cointegrated_pairs
      
